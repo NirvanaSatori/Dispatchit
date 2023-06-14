c7cb5264e3755054b5e1c34ea44c72b21e29c7ca
NirvanaSatori <morris24kariuki@gmail.com>
2023-06-14 03:40:38 +0300

    add py-padding to info component

--- frontend/src/components/Info.jsx
+++ frontend/src/components/Info.jsx
@@ -35,7 +35,7 @@ const features = [
 
 export default function Info() {
   return (
-    <div className="bg-white py-12 sm:py-32">
+    <div className="bg-white py-12 sm:py-6">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center">
           <h2 className="text-lg font-semibold leading-8 tracking-tight text-indigo-600">
