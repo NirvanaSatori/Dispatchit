82c2408b322ca39a6e737a8d7b31b62e13b69e22
NirvanaSatori <morris24kariuki@gmail.com>
2023-06-19 01:49:48 +0300

    change cors middleware origins

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://dispatchit.onrender.com"
+    origins "https://dispatchit.netlify.app"
 
     resource "*",
       headers: :any,
