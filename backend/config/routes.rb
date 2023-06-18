4a421e26523c84b8edd308f9f07507fb3ad6ed8a
NirvanaSatori <morris24kariuki@gmail.com>
2023-06-18 23:47:50 +0300

    change users resources

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :statuses, only: [:index, :update]
   resources :parcels, only: [:index, :show, :create, :update]
-  resources :users, only: [:index, :show, :create, :update]
+  # resources :users, only: [:index, :show, :create, :update]
+  resources :users, only: [:logged_in]
   resources :sessions, only: [:create, :destroy]
   
   post "/login", to: "sessions#create"
