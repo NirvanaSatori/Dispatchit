d96fee61f4aaea103ebead91c8bd3eb83d950c98
NirvanaSatori <morris24kariuki@gmail.com>
2023-06-19 00:07:03 +0300

    change fetch endpoint for logged-in user

--- frontend/src/slices/loggedInUserSlice.js
+++ frontend/src/slices/loggedInUserSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 
 export const getLoggedInUser = createAsyncThunk("user/getLoggedInUser", async () => {
-    return fetch("/users")
+    return fetch("/logged-in")
     .then(res => res.json());
 })
 
